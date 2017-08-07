6bdc473b7ce13d0a3a5ac48c366ac2a37c9d706e
xiongbingchao <276692580@qq.com>
2017-08-07 15:22:32 +0800

    Add sort & pagination export

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Post from './api/post.js'
 import User from './api/user.js'
 import * as Util from './lib/util.js'
 import API from './lib/api.js'
+import Sort from './lib/sort.js'
+import Pagination from './lib/sort.js'
 
 export default {
   Auth: Auth,
@@ -13,5 +15,7 @@ export default {
   Post: Post,
   User: User,
   Util: Util,
-  API: API
+  API: API,
+  Sort: Sort,
+  Pagination: Pagination
 }
